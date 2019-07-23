02c4e32c9c6836d1dccbe992400b21fe7c46abed
wizcheng <wizcheng@gmail.com>
2019-07-23 16:16:25 +0800

    allow react routers pass though

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/monit', monitApi);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'client/build')));
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname + 'client/build/index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client/build/index.html')));
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
